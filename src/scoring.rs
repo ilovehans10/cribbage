6473b1ac1e1e9cea07285827eaee564b8d7d4dce
Hans Larsson <hanandlia@gmail.com>
2026-01-01 18:12:57 -0600

    refactor scorer for adding tests
    Signed-off-by: Hans Larsson <hanandlia@gmail.com>

--- src/scoring.rs
+++ src/scoring.rs
@@ -8,28 +8,33 @@ pub struct Scorer {
 
 impl Scorer {
     pub fn rules_for_show() -> std::vec::Vec<Scorer> {
-        vec![
-            Scorer {
-                name: String::from("15"),
-                rule: Box::new(|deck: &Vec<Card>| {
-                    deck.iter()
-                        .powerset()
-                        .map(|x| x.iter().fold(0, |a, b| a + b.to_value()))
-                        .filter(|x| x == &15)
-                        .count()
-                        * 2
-                }),
-            },
-            Scorer {
-                name: String::from("Pair"),
-                rule: Box::new(|deck: &Vec<Card>| {
-                    deck.iter()
-                        .combinations(2)
-                        .filter(|x| x[0].value == x[1].value)
-                        .count()
-                        * 2
-                }),
-            },
-        ]
+        vec![Scorer::solver_15(), Scorer::solver_pair()]
+    }
+
+    pub(super) fn solver_15() -> Scorer {
+        Scorer {
+            name: String::from("15"),
+            rule: Box::new(|deck: &Vec<Card>| {
+                deck.iter()
+                    .powerset()
+                    .map(|x| x.iter().fold(0, |a, b| a + b.to_value()))
+                    .filter(|x| x == &15)
+                    .count()
+                    * 2
+            }),
+        }
+    }
+
+    pub(super) fn solver_pair() -> Scorer {
+        Scorer {
+            name: String::from("Pair"),
+            rule: Box::new(|deck: &Vec<Card>| {
+                deck.iter()
+                    .combinations(2)
+                    .filter(|x| x[0].value == x[1].value)
+                    .count()
+                    * 2
+            }),
+        }
     }
 }
